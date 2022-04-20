13a6850cfd309ba4871c6678535a9f1a23c76546
HIVE-LT <821721535@qq.com>
2022-04-20 17:43:13 +0800

    添加手写后端模块,还没应用到框架中

---
+++ include/backend/mybackend/backend.h
@@ -0,0 +1,19 @@
+//
+// Created by gaoxiang19 on 11/17/18.
+//
+
+#ifndef SLAM_COURSE_BACKEND_H
+#define SLAM_COURSE_BACKEND_H
+
+#include "problem.h"
+#include "vertex.h"
+#include "edge.h"
+#include "loss_function.h"
+
+#include "vertex_pose.h"
+#include "vertex_point_xyz.h"
+
+#include "edge_reprojection.h"
+
+
+#endif //SLAM_COURSE_BACKEND_H
